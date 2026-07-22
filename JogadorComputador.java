ef4a9b80dccf6e125cf7bd106585ae1d2e352bdb
enio sampaio <eniobernardo@hotmail.com>
2026-07-22 23:22:46 +0000

    Enviar um novo arquivo por upload

---
+++ JogadorComputador.java
@@ -0,0 +1,33 @@
+package inicio;
+import java.util.Random;
+
+public class JogadorComputador extends Jogador{
+		private Random random = new Random();
+		
+		public JogadorComputador(String nome, char simbolo) {
+			super(nome, simbolo);
+		}
+		
+		@Override
+		public  Jogada escolherJogada(Tabuleiro tabuleiro) {
+			
+	        System.out.println("Computador pensando...");
+
+			
+			try {
+	            Thread.sleep(1500);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+
+			int linha;
+			int coluna;
+			
+			
+			do {
+				linha = random.nextInt(3);
+				coluna = random.nextInt(3);
+			}while(!tabuleiro.casaLivre(linha, coluna));
+			return new Jogada(linha,coluna);
+}
+}
