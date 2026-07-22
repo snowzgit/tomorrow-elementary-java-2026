e80d66395b63ab1314f293cb70b71d1d61fbfec0
enio sampaio <eniobernardo@hotmail.com>
2026-07-22 23:21:34 +0000

    Substituir Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ package inicio;
 public class Main {
 	public static void main(String[] args) {
 		
-		Jogador jogador1 = new Jogador("Jogador 1", 'X');
-		Jogador jogador2 = new Jogador("Jogador 2", 'O');
+		Jogador jogador1 = new JogadorHumano("Enio", 'X');
+		Jogador jogador2 = new JogadorComputador("CPU 2", 'O');
 		
 		Tabuleiro tabuleiro = new Tabuleiro();
 		Partida partida = new Partida(tabuleiro,jogador1,jogador2);
