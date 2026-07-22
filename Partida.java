f1fb1e3acdfd2d4c9840bab88beae24186d436b0
enio sampaio <eniobernardo@hotmail.com>
2026-07-22 23:21:59 +0000

    Substituir Partida.java

--- Partida.java
+++ Partida.java
@@ -1,82 +1,74 @@
-package inicio;
-import java.util.Scanner;
-
-public class Partida {
-	private Tabuleiro tabuleiro;
-    private Jogador jogador1;
-    private Jogador jogador2;
-	private Jogador jogadorAtual;
+	package inicio;
 	
-	Scanner sc = new Scanner(System.in);
-
-	public Partida(Tabuleiro tabuleiro, Jogador jogador1, Jogador jogador2) {
-		this.tabuleiro = tabuleiro;
-		this.jogador1 = jogador1;
-        this.jogador2 = jogador2;
-        
-		this.jogadorAtual = jogador1;
-	}
-	public void partidaIniciar() {
-		while(true) {
-			System.out.println();
-			
-			tabuleiro.mostrarTabuleiro();
+	public class Tabuleiro {
 		
-			System.out.println();
 		
-			Jogador atual = getJogadorAtual();
-			
-			System.out.println("===== VEZ DO JOGADOR " + atual.getNome() +" '" + atual.getSimbolo() + "'" + " =====");	
-			
-			Jogada jogada = jogadorAtual.escolherJogada(tabuleiro);
-			
-			if(!jogada.ehValida()) {
-				System.out.print("Posições Inválidas");
-				continue;
-				}
-				
-			int linha = jogada.getLinha();
-			int coluna = jogada.getColuna();
-			if(fazerJogada(linha, coluna)) {
-				if(terminou()) {
-					tabuleiro.mostrarTabuleiro();
-					System.out.println("Jogador "+ atual.getNome() +", " + atual.getSimbolo() + " Venceu!");
-				break;
+		private char[][] casas;
+		public Tabuleiro() {
+			casas = new char[3][3];
+			for(int linha = 0; linha < 3; linha++) {
+				for(int coluna = 0; coluna < 3; coluna++)
+					casas[linha][coluna] = ' ';	
 			}
-				
-			if(tabuleiro.empate()) {
-				tabuleiro.mostrarTabuleiro();
-				System.out.println("Empate!");
-				break;
-			}
-			trocarJogador();
-			} else {
-			    System.out.println("Casa ocupada!");
+		}
+		public void mostrarTabuleiro() {
+			for (int linha = 0; linha < 3; linha++) {
+	            for (int coluna = 0; coluna < 3; coluna++) {
+	                System.out.print(casas[linha][coluna]);
+	                
+					if (coluna < 2) {
+						System.out.print(" | ");
 					}
 				}
-	}
+				System.out.println();
 	
-	public boolean fazerJogada(int linha, int coluna) {
-		if(tabuleiro.casaLivre(linha, coluna)) {
-			
-			tabuleiro.jogar(linha, coluna, jogadorAtual.getSimbolo());
-			return true;
+				if (linha < 2) {
+					System.out.println("--+---+--");
+				}
+			}
 		}
-		return false;
-	}
 	
-	public Jogador getJogadorAtual() {
-		return jogadorAtual;
-	}
-	public void trocarJogador() {
-		 if(jogadorAtual == jogador1) {
-	            jogadorAtual = jogador2;
-	        } else {
-	            jogadorAtual = jogador1;
-	        }
-	}
-
-	public boolean terminou() {
-		return tabuleiro.verificarVitoria();
-	}
+		public  boolean verificarVitoria() {
+			for (int linha = 0; linha < 3; linha++) {
+				if (casas[linha][0] != ' ' && 
+					casas[linha][0] == casas[linha][1] && 
+					casas[linha][0] == casas[linha][2]) {
+					return true;
+				}
+			}
+			for (int coluna = 0; coluna < 3; coluna++) {
+				if (casas[0][coluna] != ' ' && 
+					casas[0][coluna] == casas[1][coluna] &&
+					casas[0][coluna] == casas[2][coluna]) {
+					return true;
+				}
+			}
+					if (casas[0][0]!=' ' &&
+						casas[0][0] == casas[1][1] &&
+						casas[0][0] == casas[2][2]) {
+						return true;
+				}
+					if (casas[0][2]!=' ' &&
+						casas[0][2] == casas[1][1] &&
+						casas[0][2] == casas[2][0])
+							return true;
+	return false;
+		}
+		public void jogar(int linha, int coluna, char jogador) {
+			casas[linha][coluna] = jogador;
+		}
+		public boolean casaLivre(int linha, int coluna) {
+			return casas[linha][coluna] ==  ' ';
+		}
+		public boolean empate() {
+		    for(int linha = 0; linha < 3; linha++) {
+		        for(int coluna = 0; coluna < 3; coluna++) {
+		        	
+		        	if (casas[linha][coluna] == ' ') {
+		        		return false;
+		        	}
+		        	}
+		}
+		return true;
+		}
 }
